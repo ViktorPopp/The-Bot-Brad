d59c4eaab63ac1069119d1459c193050310d3a28
Viktor Popp <ViktorPopp.dev@gmail.com>
2024-08-15 12:05:08 +0200

    Don't work

--- responses.py
+++ responses.py
@@ -1,5 +1,7 @@
 import openai
+import dotenv
 
+dotenv.load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def get_response(message: str) -> str:
@@ -10,6 +12,7 @@ def get_response(message: str) -> str:
             model = 'gpt3.5-turbo',
             messages = [{'role': 'user', 'content': message}]
         )
+        return response.choices[0].text.strip()
 
     except:
         return 'Sorry, something went wrong Error code: 1. Contact the owner of this server for help.'
