a22d237d9fa70c5333efe19343b09e8a76442bc1
Viktor Popp <ViktorPopp.dev@gmail.com>
2024-08-17 21:32:35 +0200

    Not working

--- responses.py
+++ responses.py
@@ -3,11 +3,16 @@ from langchain_core.prompts import ChatPromptTemplate
 
 ai_model = OllamaLLM(model = "llama3.1")
 ai_template = """
+user: {ai_user}
+Chat history: {ai_chat_history}
 Answer the user input.
 User input: {ai_input}
 """
 ai_prompt = ChatPromptTemplate.from_template(ai_template)
 ai_chain = ai_prompt | ai_model
+ai_chat_history = ""
 
 def get_response(message: str) -> str:
-    return ai_chain.invoke({"ai_input": message})
+    response = ai_chain.invoke({"ai_chat_history": ai_chat_history, "ai_input": message})
+    ai_chat_history += f"\nuser: {message}\n \nYou: {response}"
+    return response
